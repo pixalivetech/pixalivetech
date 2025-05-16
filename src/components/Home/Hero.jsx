a13ac521bd92914ad76f401a796e78674d3cd03b
kvijay1710 <Vijay@Pixalive.me>
2025-05-16 17:59:38 +0530

    home hero

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -29,31 +29,28 @@ const Hero = () => {
       <div className="max-w-7xl mx-auto">
         {/* Heading */}
         <h1 className="font-lufga font-normal text-[26px] leading-[130%] sm:text-[32px] sm:leading-[140%] md:text-[40px] md:leading-[150%] lg:text-[52px] lg:leading-[160%] tracking-tight mb-8">
-          Pixalive Technology Service is one of the &nbsp;
+          Pixalive Technology Services is one of India’s   &nbsp;
           <br className="hidden sm:block" />
-          India’s fastest growing Software
+         fastest-growing tech companies, now pioneering
           <br className="hidden md:block" />
-          Development Company.
+          live streaming and digital innovation.
         </h1>
 
         {/* Paragraphs */}
         <div className="grid gap-6 sm:gap-8 md:grid-cols-3 mt-[50px] sm:text-[15px] md:text-base">
           <p className="font-normal text-[18px] leading-[160%] tracking-normal hidden sm:block"></p>
           <p className="font-normal text-[18px] leading-[170%] tracking-normal">
-            Pixalive Technology Services builds smart, scalable tech solutions
-            to power businesses and the Pixalive Ecosystem.
+           Pixalive Technology Services creates scalable tech solutions to support businesses and the Pixalive Ecosystem. Their latest innovation, live streaming, transforms audience engagement.
           </p>
 
           <p className="font-normal text-[18px] leading-[170%] tracking-normal">
-            Pixalive Technology Services offers innovative software, cloud, and
-            consulting solutions. We power the Pixalive Ecosystem and help
-            businesses grow through tech.
+            Pixalive Technology Services delivers software, cloud, and consulting solutions to boost business growth. Their advanced live streaming tech enhances digital engagement for global brands
           </p>
         </div>
         <div className="mt-20 flex justify-center">
           <img
             ref={imgRef}
-            className={`w-full sm:max-w-[1260px] h-auto transition-transform duration-[1500ms] ease-in-out transform ${
+            className={`w-full sm:max-w-[1260px] h-auto rounded-[12px] transition-transform duration-[1500ms] ease-in-out transform ${
               isVisible ? "scale-100 opacity-100" : "scale-95 opacity-0"
             }`}
             src={hero}
